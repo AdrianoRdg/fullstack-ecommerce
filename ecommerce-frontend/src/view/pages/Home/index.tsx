ac1604a389cf913c4a35a52ca01d8a1d623c2b2d
Adriano Rodrigues <adrianoo.rdg@gmail.com>
2024-04-15 09:25:50 -0300

    começando section de browse

--- ecommerce-frontend/src/view/pages/Home/index.tsx
+++ ecommerce-frontend/src/view/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from '../../components/Header';
+import { Browse } from './sections/Browse';
 import { Hero } from './sections/Hero';
 
 export function Home() {
@@ -6,6 +7,7 @@ export function Home() {
     <>
       <Header />
       <Hero />
+      <Browse />
     </>
   );
 }
---
+++ ecommerce-frontend/src/view/pages/Home/sections/Browse/index.tsx
@@ -0,0 +1,3 @@
+export function Browse() {
+  return <h1>Ola</h1>;
+}
