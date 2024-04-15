76b6cd60d5e163bf6e2acf57b8d998255ee41522
Adriano Rodrigues <adrianoo.rdg@gmail.com>
2024-04-15 10:50:46 -0300

    feat: feito container dos cards de browse e criação dos cards (hard code)

--- ecommerce-frontend/src/view/pages/Home/sections/Browse/index.tsx
+++ ecommerce-frontend/src/view/pages/Home/sections/Browse/index.tsx
@@ -1,3 +1,16 @@
+import { BrowseCard } from '../../../../components/Cards/BrowseCard';
+import { BrowseCards, BrowseContainer } from './styles';
+
 export function Browse() {
-  return <h1>Ola</h1>;
+  return (
+    <BrowseContainer className="container">
+      <h2>Browse The Range</h2>
+
+      <BrowseCards>
+        <BrowseCard />
+        <BrowseCard />
+        <BrowseCard />
+      </BrowseCards>
+    </BrowseContainer>
+  );
 }
